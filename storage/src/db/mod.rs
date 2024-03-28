3abec00e4b62788ea02e4f3f0ef40e9beff7525a
Anthony <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-03-28 15:43:19 -0700

    Refactor database initialization logic
    ensure we only initialize the DB once

--- storage/src/db/mod.rs
+++ storage/src/db/mod.rs
@@ -4,17 +4,26 @@ pub mod schemas;
 use std::path::PathBuf;
 
 use log::info;
-use once_cell::sync::Lazy;
+use once_cell::sync::{Lazy, OnceCell};
 use surrealdb::engine::local::{Db, SpeeDb};
 use surrealdb::Surreal;
 
+static INIT: OnceCell<Option<()>> = OnceCell::new();
 pub(crate) static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
 
 pub async fn init_database(path: PathBuf) -> surrealdb::Result<()> {
-    DB.connect::<SpeeDb>(path).await?;
-    DB.use_ns("mecomp").await?;
-    DB.use_db("music").await?;
-    info!("Connected to music database");
+    let previously_initialized = INIT.get().is_some();
+
+    if !previously_initialized {
+        let Ok(_) = INIT.set(Some(())) else {
+            return Ok(());
+        };
+
+        DB.connect::<SpeeDb>(path).await?;
+        DB.use_ns("mecomp").await?;
+        DB.use_db("music").await?;
+        info!("Connected to music database");
+    }
 
     Ok(())
 }
