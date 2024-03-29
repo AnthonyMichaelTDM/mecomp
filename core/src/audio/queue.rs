faf6b3195762a7bc2c0edc69234df9b32e335694
Anthony <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-03-28 18:12:02 -0700

    Refactor code to improve performance and readability

--- core/src/audio/queue.rs
+++ core/src/audio/queue.rs
@@ -7,7 +7,7 @@ use mecomp_storage::db::schemas::song::Song;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Queue {
     songs: Vec<Song>,
-    current_index: usize,
+    current_index: Option<usize>,
     repeat_mode: RepeatMode,
 }
 
@@ -22,7 +22,7 @@ impl Queue {
     pub const fn new() -> Self {
         Self {
             songs: Vec::new(),
-            current_index: 0,
+            current_index: None,
             repeat_mode: RepeatMode::None,
         }
     }
@@ -32,22 +32,37 @@ impl Queue {
     }
 
     pub fn remove_song(&mut self, index: usize) {
-        // TODO: if index is current_index, update current_index
-        if index < self.current_index || (index == self.current_index && self.current_index != 0) {
-            self.current_index -= 1;
+        if index >= self.len() {
+            return;
         }
+
+        match self.current_index {
+            Some(current_index)
+                if current_index > index || (current_index == index && current_index > 0) =>
+            {
+                self.current_index = Some(current_index - 1);
+            }
+            Some(_) if self.len() <= 1 => {
+                self.current_index = None;
+            }
+            _ => {}
+        }
+
         self.songs.remove(index);
     }
 
     pub fn clear(&mut self) {
         // TODO: stop playback
         self.songs.clear();
-        self.current_index = 0;
+        self.current_index = None;
     }
 
     #[must_use]
     pub fn current_song(&self) -> Option<&Song> {
-        self.songs.get(self.current_index)
+        match self.current_index {
+            Some(index) => self.songs.get(index),
+            None => None,
+        }
     }
 
     pub fn next_song(&mut self) -> Option<&Song> {
@@ -55,37 +70,77 @@ impl Queue {
         if self.songs.is_empty() {
             return None;
         }
+        if self.current_index.is_none() {
+            self.current_index = Some(0);
+            return self.songs.get(0);
+        }
+
+        let mut current_index = self.current_index.unwrap_or(0);
 
         // now, increment the index depending on the repeat mode
         match self.repeat_mode {
             RepeatMode::None => {
-                if self.current_index + 1 < self.songs.len() {
-                    self.current_index += 1;
+                if current_index + 1 < self.songs.len() {
+                    current_index += 1;
                 }
             }
             RepeatMode::Once => {
-                if self.current_index + 1 < self.songs.len() {
-                    self.current_index += 1;
+                if current_index + 1 < self.songs.len() {
+                    current_index += 1;
                 } else {
-                    self.current_index = 0;
+                    current_index = 0;
                     self.repeat_mode = RepeatMode::None;
                 }
             }
             RepeatMode::Continuous => {
-                self.current_index = (self.current_index + 1) % self.songs.len();
+                current_index = (current_index + 1) % self.songs.len();
             }
         }
 
+        self.current_index = Some(current_index);
+
         // return the current song
-        self.songs.get(self.current_index)
+        self.songs.get(current_index)
+    }
+
+    /// Skip n songs
+    pub fn skip_song(&mut self, n: usize) -> Option<&Song> {
+        // if we can skip n songs without reaching the end of the queue
+        match self.current_index {
+            Some(current_index) if current_index + n < self.songs.len() => {
+                self.current_index = Some(current_index + n);
+                self.current_index.and_then(|index| self.songs.get(index))
+            }
+            _ => match self.repeat_mode {
+                RepeatMode::None => {
+                    if self.current_index == Some(self.songs.len() - 1) {
+                        return None;
+                    }
+                    self.current_index = Some(self.songs.len() - 1);
+                    self.current_index.and_then(|index| self.songs.get(index))
+                }
+                RepeatMode::Once => {
+                    self.current_index = Some(0);
+                    self.repeat_mode = RepeatMode::None;
+                    self.current_index.and_then(|index| self.songs.get(index))
+                }
+                RepeatMode::Continuous => {
+                    self.current_index = self
+                        .current_index
+                        .map(|index| (index + n) % self.songs.len());
+                    self.current_index.and_then(|index| self.songs.get(index))
+                }
+            },
+        }
     }
 
     pub fn previous_song(&mut self) -> Option<&Song> {
-        if self.current_index > 0 {
-            self.current_index -= 1;
-            self.songs.get(self.current_index)
-        } else {
-            None
+        match self.current_index {
+            Some(current_index) if current_index > 0 => {
+                self.current_index = Some(current_index - 1);
+                self.songs.get(current_index - 1)
+            }
+            _ => None,
         }
     }
 
@@ -97,6 +152,25 @@ impl Queue {
         // shuffle
         self.songs.shuffle(&mut thread_rng());
         // swap current song to first
+        match self.current_index {
+            Some(current_index) if current_index != 0 => {
+                self.songs.swap(0, current_index);
+                self.current_index = Some(0);
+            }
+            _ => {}
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&Song> {
+        self.songs.get(index)
+    }
+
+    pub fn len(&self) -> usize {
+        self.songs.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.songs.is_empty()
         if self.current_index != 0 {
             self.songs.swap(0, self.current_index);
             self.current_index = 0;
