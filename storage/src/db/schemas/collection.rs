84ae233c1b29967baaec15905a4b8885753a5b0c
Anthony <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-04-07 00:35:43 -0700

    add name field to collection schema

--- storage/src/db/schemas/collection.rs
+++ storage/src/db/schemas/collection.rs
@@ -1,5 +1,7 @@
 //! A collection is an auto currated list of similar songs.
 
+use std::sync::Arc;
+
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::{Duration, Id, Thing};
 use surrealqlx::Table;
@@ -14,6 +16,10 @@ pub struct Collection {
     /// the unique identifier for this [`Collection`].
     #[field(dt = "record")]
     pub id: CollectionId,
+
+    /// The name of the collection.
+    #[field(dt = "string", index(unique))]
+    pub name: Arc<str>,
 
     /// Total runtime.
     #[field(dt = "duration")]
@@ -30,9 +36,16 @@ impl Collection {
     }
 }
 
+#[derive(Debug, Default, Serialize)]
+pub struct CollectionChangeSet {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<Arc<str>>,
+}
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CollectionBrief {
     pub id: CollectionId,
+    pub name: Arc<str>,
     pub runtime: std::time::Duration,
     pub songs: usize,
 }
@@ -41,6 +54,7 @@ impl From<Collection> for CollectionBrief {
     fn from(collection: Collection) -> Self {
         Self {
             id: collection.id,
+            name: collection.name,
             runtime: collection.runtime.into(),
             songs: collection.song_count,
         }
@@ -51,6 +65,7 @@ impl From<&Collection> for CollectionBrief {
     fn from(collection: &Collection) -> Self {
         Self {
             id: collection.id.clone(),
+            name: collection.name.clone(),
             runtime: collection.runtime.into(),
             songs: collection.song_count,
         }
