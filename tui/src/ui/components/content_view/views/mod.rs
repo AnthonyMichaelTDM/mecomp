aaa13acb75639e29d87e2429f86747a696c9abda
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-06-19 18:46:12 -0700

    refactor(tui): Simplify creation of tree widgets
    by extracting repeated code into utils module

--- tui/src/ui/components/content_view/views/mod.rs
+++ tui/src/ui/components/content_view/views/mod.rs
@@ -5,3 +5,96 @@ pub mod none;
 pub mod playlist;
 pub mod search;
 pub mod song;
+
+pub mod utils {
+    use mecomp_storage::db::schemas::{album::Album, artist::Artist, song::Song};
+    use ratatui::{
+        style::{Style, Stylize},
+        text::{Line, Span},
+    };
+    use tui_tree_widget::TreeItem;
+
+    pub fn create_album_tree_item(albums: &[Album]) -> Result<TreeItem<String>, std::io::Error> {
+        TreeItem::new(
+            "Albums".to_string(),
+            format!("Albums ({}):", albums.len()),
+            albums
+                .iter()
+                .map(|album| create_album_tree_leaf(album, None))
+                .collect(),
+        )
+    }
+
+    pub fn create_album_tree_leaf<'a>(
+        album: &Album,
+        prefix: Option<Span<'a>>,
+    ) -> TreeItem<'a, String> {
+        TreeItem::new_leaf(
+            album.id.to_string(),
+            Line::from(vec![
+                prefix.unwrap_or_default(),
+                Span::styled(album.title.to_string(), Style::default().bold()),
+                Span::raw(" "),
+                Span::styled(
+                    album
+                        .artist
+                        .iter()
+                        .map(ToString::to_string)
+                        .collect::<Vec<String>>()
+                        .join(", "),
+                    Style::default().italic(),
+                ),
+            ]),
+        )
+    }
+
+    pub fn create_artist_tree_item(artists: &[Artist]) -> Result<TreeItem<String>, std::io::Error> {
+        TreeItem::new(
+            "Artists".to_string(),
+            format!("Artists ({}):", artists.len()),
+            artists
+                .iter()
+                .map(|artist| create_artist_tree_leaf(artist))
+                .collect(),
+        )
+    }
+
+    pub fn create_artist_tree_leaf(artist: &Artist) -> TreeItem<String> {
+        TreeItem::new_leaf(
+            artist.id.to_string(),
+            Line::from(vec![Span::styled(
+                artist.name.to_string(),
+                Style::default().bold(),
+            )]),
+        )
+    }
+
+    pub fn create_song_tree_item(songs: &[Song]) -> Result<TreeItem<String>, std::io::Error> {
+        TreeItem::new(
+            "Songs".to_string(),
+            format!("Songs ({}):", songs.len()),
+            songs
+                .iter()
+                .map(|song| create_song_tree_leaf(song))
+                .collect(),
+        )
+    }
+
+    pub fn create_song_tree_leaf(song: &Song) -> TreeItem<String> {
+        TreeItem::new_leaf(
+            song.id.to_string(),
+            Line::from(vec![
+                Span::styled(song.title.to_string(), Style::default().bold()),
+                Span::raw(" "),
+                Span::styled(
+                    song.artist
+                        .iter()
+                        .map(ToString::to_string)
+                        .collect::<Vec<String>>()
+                        .join(", "),
+                    Style::default().italic(),
+                ),
+            ]),
+        )
+    }
+}
--- tui/src/ui/components/content_view/views/search.rs
+++ tui/src/ui/components/content_view/views/search.rs
@@ -7,21 +7,22 @@ use mecomp_core::rpc::SearchResult;
 use mecomp_storage::db::schemas::Thing;
 use ratatui::{
     layout::{Constraint, Direction, Layout},
-    style::{Color, Modifier, Style, Stylize},
-    text::{Line, Span},
+    style::{Color, Modifier, Style},
     widgets::Block,
 };
 use tokio::sync::mpsc::UnboundedSender;
-use tui_tree_widget::{Tree, TreeItem, TreeState};
+use tui_tree_widget::{Tree, TreeState};
 
 use crate::{
-    state::action::{Action, AudioAction, QueueAction},
+    state::action::Action,
     ui::{
         components::{Component, ComponentRender, RenderProps},
         widgets::searchbar::{self, SearchBar},
         AppState,
     },
 };
+
+use super::utils::{create_album_tree_item, create_artist_tree_item, create_song_tree_item};
 
 #[allow(clippy::module_name_repetitions)]
 pub struct SearchView {
@@ -83,7 +84,6 @@ impl Component for SearchView {
     }
 
     fn handle_key_event(&mut self, key: crossterm::event::KeyEvent) {
-        // defer to the search box (except for the up and down keys)
         match key.code {
             // arrow keys
             KeyCode::Up => {
@@ -114,7 +114,6 @@ impl Component for SearchView {
             // when searchbar unfocused, enter key will open the selected node
             KeyCode::Enter if !self.search_bar_focused => {
                 if self.tree_state.lock().unwrap().toggle_selected() {
-                    // TODO: instead of just adding to the queue, instead open the view for the selected item
                     let things: Vec<Thing> = self
                         .tree_state
                         .lock()
@@ -124,8 +123,10 @@ impl Component for SearchView {
                         .filter_map(|id| id.parse::<Thing>().ok())
                         .collect();
                     if !things.is_empty() {
+                        debug_assert!(things.len() == 1);
+                        let thing = things[0].clone();
                         self.action_tx
-                            .send(Action::Audio(AudioAction::Queue(QueueAction::Add(things))))
+                            .send(Action::SetCurrentView(thing.into()))
                             .unwrap();
                     }
                 }
@@ -150,80 +151,9 @@ impl ComponentRender<RenderProps> for SearchView {
         };
 
         // create list to hold results
-        let song_tree = TreeItem::new(
-            "Songs".to_string(),
-            format!("Songs ({}):", self.props.search_results.songs.len()),
-            self.props
-                .search_results
-                .songs
-                .iter()
-                .map(|song| {
-                    TreeItem::new_leaf(
-                        song.id.to_string(),
-                        Line::from(vec![
-                            Span::styled(song.title.to_string(), Style::default().bold()),
-                            Span::raw(" "),
-                            Span::styled(
-                                song.artist
-                                    .iter()
-                                    .map(ToString::to_string)
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                Style::default().italic(),
-                            ),
-                        ]),
-                    )
-                })
-                .collect(),
-        )
-        .unwrap();
-        let album_tree = TreeItem::new(
-            "Albums".to_string(),
-            format!("Albums ({}):", self.props.search_results.albums.len()),
-            self.props
-                .search_results
-                .albums
-                .iter()
-                .map(|album| {
-                    TreeItem::new_leaf(
-                        album.id.to_string(),
-                        Line::from(vec![
-                            Span::styled(album.title.to_string(), Style::default().bold()),
-                            Span::raw(" "),
-                            Span::styled(
-                                album
-                                    .artist
-                                    .iter()
-                                    .map(ToString::to_string)
-                                    .collect::<Vec<String>>()
-                                    .join(", "),
-                                Style::default().italic(),
-                            ),
-                        ]),
-                    )
-                })
-                .collect(),
-        )
-        .unwrap();
-        let artist_tree = TreeItem::new(
-            "Artists".to_string(),
-            format!("Artists ({}):", self.props.search_results.artists.len()),
-            self.props
-                .search_results
-                .artists
-                .iter()
-                .map(|artist| {
-                    TreeItem::new_leaf(
-                        artist.id.to_string(),
-                        Line::from(vec![Span::styled(
-                            artist.name.to_string(),
-                            Style::default().bold(),
-                        )]),
-                    )
-                })
-                .collect(),
-        )
-        .unwrap();
+        let song_tree = create_song_tree_item(&self.props.search_results.songs).unwrap();
+        let album_tree = create_album_tree_item(&self.props.search_results.albums).unwrap();
+        let artist_tree = create_artist_tree_item(&self.props.search_results.artists).unwrap();
         let items = &[song_tree, album_tree, artist_tree];
 
         let [search_bar_area, results_area] = *Layout::default()
