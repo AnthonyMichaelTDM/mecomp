66b1cefaf8ef578488d48ded27b89034916a86db
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2025-12-02 02:24:37 -0600

    chore:fmt

--- analysis/src/chroma.rs
+++ analysis/src/chroma.rs
@@ -266,7 +266,9 @@ pub fn pip_track(
 
     // Validate that a valid frequency range was found
     if beginning >= end {
-        return Err(AnalysisError::AnalysisError(String::from("in chroma: no valid frequency range found")));
+        return Err(AnalysisError::AnalysisError(String::from(
+            "in chroma: no valid frequency range found",
+        )));
     }
     // There will be at most taken_columns * length elements in pitches / mags
     let mut pitches = Vec::with_capacity(taken_columns * length);
